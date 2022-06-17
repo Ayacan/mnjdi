028783953726bbdd3ef52f8e720caa97cc06e828
Prili kawaii <97828582+Ayacan@users.noreply.github.com>
2022-06-17 23:00:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ global.urlnya = "https://www.instagram.com/aguzfamilia"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 857-1771-2623'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
